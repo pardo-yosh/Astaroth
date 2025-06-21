c22ef19552b411ad704408ba19977b9435951363
pardo-yosh <yoshpardomedina@gmail.com>
2025-06-21 01:57:20 -0500

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 import { loadComponent } from "/Astaroth/assets/js/components/initComponents.js";
-import { setupSidebar, setupDarkMode } from "/assets/js/core/visual.js";
+import {
+  setupSidebar,
+  setupDarkMode,
+} from "/Astaroth/assets/js/core/visual.js";
 
 async function initializeApp() {
   // 1. Primero carga TODOS los componentes
   await Promise.all([
-    loadComponent("/components/header.html", "header-placeholder"),
-    loadComponent("/components/sidebar.html", "sidebar-placeholder"),
+    loadComponent("/Astaroth/components/header.html", "header-placeholder"),
+    loadComponent("/Astaroth/components/sidebar.html", "sidebar-placeholder"),
   ]);
 
   // 2. Luego configura las funcionalidades
