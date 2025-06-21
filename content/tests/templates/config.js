b84f7cbfa4b633e0bcf864b93383c5690da140a7
pardo-yosh <yoshpardomedina@gmail.com>
2025-06-21 13:23:25 -0500

    modified:   content/tests/templates/config.js

--- content/tests/templates/config.js
+++ content/tests/templates/config.js
@@ -4,12 +4,15 @@ export const QUIZ_PATHS = {
   automatizacion: {
     tema6: "/Astaroth/content/tests/questions/automatizacion/tema6.json",
     tema7: "/Astaroth/content/tests/questions/automatizacion/tema7.json",
+    tema8: "/Astaroth/content/tests/questions/automatizacion/tema8.json",
   },
   control: {
     clase8: "/Astaroth/content/tests/questions/control/clase8.json",
     clase10: "/Astaroth/content/tests/questions/control/clase10.json",
     clase11: "/Astaroth/content/tests/questions/control/clase11.json",
     clase12: "/Astaroth/content/tests/questions/control/clase12.json",
+    clase13: "/Astaroth/content/tests/questions/control/clase13.json",
+    laboratorio8: "/Astaroth/content/tests/questions/control/laboratorio8.json",
   },
   procesamiento: {
     clase8: "/Astaroth/content/tests/questions/procesamiento/clase8.json",
